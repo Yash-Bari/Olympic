cb7acff2cc929545572ad379f11b43ecce962e06
YASH  BARI <128377505+Yash-Bari@users.noreply.github.com>
2023-07-25 15:59:47 +0530

    Update Athlete.py

--- Athlete.py
+++ Athlete.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-# Load the CSV file
 url = 'https://raw.githubusercontent.com/Yash-Bari/dataset/main/Athlete.csv'
 df = pd.read_csv(url)
 
-# Function to display athlete details on click
 def display_details(row):
     st.image(row['Athlete_image_url'], width=150, output_format='PNG')
     st.header(row['Athlete_name'])
@@ -17,7 +15,6 @@ def display_details(row):
     st.subheader(row['Event name'])
     st.markdown(f"**Medal:** {row['Medal']}")
 
-# Main Streamlit app
 def main():
     st.set_page_config(
         layout="wide",
@@ -26,14 +23,12 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    # Add a smaller header and separation
     header_container = st.container()
     with header_container:
         st.markdown("<h1 style='text-align: center; font-size: 50px;'>Olympic Athlete Information</h1>", unsafe_allow_html=True)
         st.markdown("---")
         st.markdown("Explore details of Olympic athletes and their achievements!")
-
-    # Display athlete information with minimize feature
+        
     for _, row in df.iterrows():
         col1, col2, col3, col4 = st.columns([1, 1, 2, 1])
         with col1:
