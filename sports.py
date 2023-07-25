f15f6b7c04ea65aa0d0c927efbd3c8df5bc0cbc7
YASH  BARI <128377505+Yash-Bari@users.noreply.github.com>
2023-07-25 16:11:30 +0530

    Update sports.py

--- sports.py
+++ sports.py
@@ -4,16 +4,12 @@ from translate import Translator
 from gtts import gTTS
 import os
 
-
-# Function to generate audio using Gtts
 def generate_audio(text, lang='en'):
     tts = gTTS(text=text, lang=lang)
     audio_file = 'temp_audio.mp3'
     tts.save(audio_file)
     return audio_file
 
-
-# Function to split text into chunks with complete sentences
 def split_text(text, chunk_size=500):
     chunks = []
     words = text.split(' ')
@@ -28,25 +24,19 @@ def split_text(text, chunk_size=500):
         chunks.append(current_chunk.strip())
     return chunks
 
-
-# Load the CSV data
 url = 'https://raw.githubusercontent.com/Yash-Bari/dataset/main/Sports.csv'
 df = pd.read_csv(url)
 
-# Set page title and icon
 st.set_page_config(page_title='Sports Information App', page_icon=':sports_medal:')
 
-# Page header with separation lines
 st.title('Sports Information App')
+st.markdown("Translate sports information in any language.", unsafe_allow_html=True)
 st.write('---')
 
-# Select a sport from the dropdown
 selected_sport = st.selectbox('Select a sport', df['Sport'].unique())
 
-# Filter the data based on the selected sport
 sport_data = df[df['Sport'] == selected_sport].iloc[0]
 
-# Display the sport image and history in the same size
 col1, col2 = st.columns(2)
 
 with col1:
@@ -56,12 +46,10 @@ with col2:
     st.subheader(selected_sport + ' History')
     st.write(sport_data['History'])
 
-# Voice Translator
-st.write('---')  # Separation line
+st.write('---') 
 st.subheader('Voice Translator')
 text_to_translate = sport_data['History']
 
-# Dictionary to map language names to their codes
 languages = {
     'English': 'en',
     'Spanish': 'es',
@@ -76,7 +64,6 @@ languages = {
     'Marathi': 'mr',
 }
 
-# Create a list of language names for the dropdown
 language_names = list(languages.keys())
 selected_language = st.selectbox('Select a language', language_names)
 
@@ -98,6 +85,4 @@ if translate_button:
 
         translated_audio = generate_audio(translated_text, lang=lang_code)
         st.audio(translated_audio, format='audio/mp3')
-
-        # Delete temporary audio file after playing
         os.remove(translated_audio)
