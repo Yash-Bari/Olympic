80e1dae90a8ffb3bc7d296159c3ab9ad55702053
YASH  BARI <128377505+Yash-Bari@users.noreply.github.com>
2023-07-25 16:02:21 +0530

    Update Medal.py

--- Medal.py
+++ Medal.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import streamlit as st
 
-# Set Streamlit wide mode to display all data
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(layout="wide")
 
-# Read the CSV file into a Pandas DataFrame
 data = pd.read_csv("medals.csv")
 
-# Add custom CSS styles
 st.markdown("""
     <style>
         body {
@@ -38,11 +35,9 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Page header
 st.title("Olympic Medal Analysis")
 st.subheader("Explore medal data for different countries and athletes")
 
-# Sidebar instructions
 st.sidebar.title("Instructions")
 st.sidebar.markdown("""
     1. Use the dropdown to select a country.
@@ -51,32 +46,25 @@ st.sidebar.markdown("""
     4. Check the overall medal distribution.
 """)
 
-# Sidebar for filtering options
 st.sidebar.header("Filter Options")
 selected_country = st.sidebar.selectbox("Select Country", data["country_name"].unique(), index=0)
 
-# Filter the data based on the selected country
 filtered_data = data[data["country_name"] == selected_country]
 
-# Medal Tally
 st.header("Medal Tally")
 st.bar_chart(filtered_data["medal_type"].value_counts())
 
-# Country-wise analysis
 st.header("Country-wise Analysis")
 country_medal_tally = filtered_data["medal_type"].value_counts().rename_axis('Medal Type').reset_index(name='Count')
 st.dataframe(country_medal_tally, height=200)
 
-# Athlete-wise analysis
 st.header("Athlete-wise Analysis")
 athlete_medal_tally = filtered_data.groupby("athlete_full_name")["medal_type"].value_counts().unstack().fillna(0)
 st.bar_chart(athlete_medal_tally)
 
-# Overall analysis
 st.header("Overall Analysis")
 overall_medal_tally = data["country_name"].value_counts()
 st.bar_chart(overall_medal_tally)
 
-# Footer
 st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: gray;'>Data Analysis</p>", unsafe_allow_html=True)
