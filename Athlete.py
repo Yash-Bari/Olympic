584d727584a1414a1d79d19bc3e53ab9562e11c2
YASH  BARI <128377505+Yash-Bari@users.noreply.github.com>
2023-07-31 01:45:13 +0530

    Update Athlete.py

--- Athlete.py
+++ Athlete.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 url = 'https://raw.githubusercontent.com/Yash-Bari/dataset/main/Athlete.csv'
 df = pd.read_csv(url)
-
+st.image("https://th.bing.com/th/id/OIP.W8FzzgXIKTGjKmHFYkdYnAAAAA?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", caption="MyOlympia", width=100)
 def display_details(row):
     st.image(row['Athlete_image_url'], width=150, output_format='PNG')
     st.header(row['Athlete_name'])
@@ -26,7 +26,6 @@ def main():
         page_icon=":runner:",
         initial_sidebar_state="expanded",
     )
-    st.image("https://th.bing.com/th/id/OIP.W8FzzgXIKTGjKmHFYkdYnAAAAA?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", caption="MyOlympia", width=100)
     header_container = st.container()
     with header_container:
         st.markdown("<h1 style='text-align: center; font-size: 50px;'>Olympic Athlete Information</h1>", unsafe_allow_html=True)
