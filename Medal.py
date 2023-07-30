e6ed66b4cb6bf38c0de0b05ac3acd552095fb289
YASH  BARI <128377505+Yash-Bari@users.noreply.github.com>
2023-07-31 01:37:00 +0530

    Update Medal.py

--- Medal.py
+++ Medal.py
@@ -34,7 +34,7 @@ st.markdown("""
         }
     </style>
 """, unsafe_allow_html=True)
-
+st.image("https://th.bing.com/th/id/OIP.W8FzzgXIKTGjKmHFYkdYnAAAAA?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", caption="MyOlympia", width=100)
 st.title("Olympic Medal Analysis")
 st.subheader("Explore medal data for different countries and athletes")
 
