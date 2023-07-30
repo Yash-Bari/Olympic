08d3ee53d87365513dc57fbc77e52eb041d19517
YASH  BARI <128377505+Yash-Bari@users.noreply.github.com>
2023-07-31 01:44:34 +0530

    Update Athlete.py

--- Athlete.py
+++ Athlete.py
@@ -26,7 +26,7 @@ def main():
         page_icon=":runner:",
         initial_sidebar_state="expanded",
     )
-     st.image("https://th.bing.com/th/id/OIP.W8FzzgXIKTGjKmHFYkdYnAAAAA?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", caption="MyOlympia", width=100)
+    st.image("https://th.bing.com/th/id/OIP.W8FzzgXIKTGjKmHFYkdYnAAAAA?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", caption="MyOlympia", width=100)
     header_container = st.container()
     with header_container:
         st.markdown("<h1 style='text-align: center; font-size: 50px;'>Olympic Athlete Information</h1>", unsafe_allow_html=True)
